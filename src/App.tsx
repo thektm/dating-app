5d23a40a045bbfc5ddcd360bb517194d687af44d
thektm <138784494+thektm@users.noreply.github.com>
2025-03-10 16:20:19 +0330

    Update App.tsx with initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import './App.css'
-
-import Counter from './components/counter'
-
 const App: React.FC = () => {
-  
 
   return (
     <>
-    
     <div className=' bg-amber-50 w-screen h-screen flex justify-center items-center'>
-      <Counter/>
       </div>
-      
     </>
   )
 }
