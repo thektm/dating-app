1b100197a05b4920d0ce646d8d36db43f219d875
thektm <138784494+thektm@users.noreply.github.com>
2025-03-10 16:52:07 +0330

    Update App.tsx with initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css'
+import {BrowserRouter , Routes , Route , } from 'react-router-dom'
+import Home from './componenets/home'
+import Contact from './componenets/contact'
+import Aboutus from './componenets/Aboutus.tsx'
 const App: React.FC = () => {
 
   return (
-    <>
-    <div className=' bg-amber-50 w-screen h-screen flex justify-center items-center'>
-      </div>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/about' element={<Aboutus />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
 export default App
